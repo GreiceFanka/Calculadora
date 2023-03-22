b1a56360c5157bca95739bc3d643cd46bb632637
GreiceFanka <greicefcardoso@gmail.com>
2023-03-22 20:42:59 +0100

    Implementacao da logica da calculadora e do tipo porcentagem

--- src/modelo/Memoria.java
+++ src/modelo/Memoria.java
@@ -10,8 +10,12 @@ public class Memoria {
 	};
 	
 	private static Memoria instancia = new Memoria();
-	private String textoAtual = "";
 	private final List<MemoriaObservador> observadores = new ArrayList<>();
+	private boolean substituir = false;
+	private String textoAtual = "";
+	private String textoBuffer = "";
+	private TipoComando ultimaOperacao = null;
+	private TipoComando ultimaOperacaoBuffer = null;
 	
 	private Memoria() {
 		
@@ -31,9 +35,72 @@ public class Memoria {
 	
 	public void processarComando(String texto) {
 		TipoComando tipoComando = detectarTipoComando(texto);
-		textoAtual += texto;
+		if(tipoComando == null) {
+			return;
+		}else if(tipoComando == TipoComando.ZERAR) {
+			textoAtual = "";
+			textoBuffer = "";
+			substituir = false;
+			ultimaOperacao = null;
+		}else if(tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
+			textoAtual = substituir ? texto : textoAtual + texto;
+			substituir = false;
+		}else if(tipoComando == TipoComando.PORCENTAGEM) {
+			try {
+				textoAtual = substituir ? texto : textoAtual + texto;
+				substituir = false;
+				ultimaOperacao = tipoComando;
+				textoAtual = obterResultadoOperacao();
+				textoBuffer = textoAtual;
+			} catch (Exception e) {
+				// Erro de formato inserido
+				textoAtual = "FormatError!";
+			}
+		}else {
+			substituir = true;
+			textoAtual = obterResultadoOperacao();
+			textoBuffer = textoAtual;
+			ultimaOperacao = tipoComando;
+			ultimaOperacaoBuffer = ultimaOperacao;
+		}
+		
 		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
-		System.out.println(tipoComando);
+		
+	}
+
+	private String obterResultadoOperacao() {
+		if(ultimaOperacao == null || ultimaOperacao == TipoComando.IGUAL) {
+			return textoAtual;
+		}
+		if(textoBuffer.contains("%") || textoAtual.contains("%")) {
+			textoAtual = textoAtual.replace("%", "");
+			textoBuffer = textoBuffer.replace("%", "");
+		}
+		double numeroBuffer = Double.parseDouble(textoBuffer.replace(",", "."));
+		double numeroAtual = Double.parseDouble(textoAtual.replace(",", "."));
+		double resultado = 0;
+		if(ultimaOperacao == TipoComando.SOMA) {
+			resultado = numeroBuffer + numeroAtual;
+		}else if(ultimaOperacao == TipoComando.SUB) {
+			resultado = numeroBuffer - numeroAtual;
+		}else if(ultimaOperacao == TipoComando.DIV) {
+			resultado = numeroBuffer / numeroAtual;
+		}else if(ultimaOperacao == TipoComando.MULTI) {
+			resultado = numeroBuffer * numeroAtual;
+		}else if(ultimaOperacao == TipoComando.PORCENTAGEM) {
+			if(ultimaOperacaoBuffer == TipoComando.SUB) {
+				resultado = numeroBuffer - (numeroBuffer * numeroAtual)/100;
+			}else if(ultimaOperacaoBuffer == TipoComando.SOMA) {
+				resultado = numeroBuffer + (numeroBuffer * numeroAtual /100);
+			}else if(ultimaOperacaoBuffer == TipoComando.DIV) {
+				resultado = numeroBuffer / (numeroBuffer * numeroAtual /100) * 100;
+			}else if(ultimaOperacaoBuffer == TipoComando.MULTI) {
+				resultado = (numeroBuffer * numeroAtual /100);
+			}
+		}
+		String resultadoString = Double.toString(resultado).replace(".", ",");
+		boolean inteiro = resultadoString.endsWith(",0");
+		return inteiro ? resultadoString.replace(",0","") : resultadoString;
 	}
 
 	private TipoComando detectarTipoComando(String texto) {
@@ -57,9 +124,9 @@ public class Memoria {
 				return TipoComando.MULTI;
 			}else if("=".equals(texto)) {
 				return TipoComando.IGUAL;
-			}else if("%".equals(texto)) {
+			}else if(texto.contains("%")) {
 				return TipoComando.PORCENTAGEM;
-			}else if(",".equals(texto)) {
+			}else if(",".equals(texto) && !textoAtual.contains(",")) {
 				return TipoComando.VIRGULA;
 			}
 		}
