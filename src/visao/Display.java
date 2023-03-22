fc417aef077e9382f55d516e5b416d98d327d2a7
GreiceFanka <greicefcardoso@gmail.com>
2023-03-22 11:57:01 +0100

    Implementacao do display como interessado nos eventos e alteracao do valor do label no display

--- src/visao/Display.java
+++ src/visao/Display.java
@@ -7,15 +7,22 @@ import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import modelo.Memoria;
+import modelo.MemoriaObservador;
+
 @SuppressWarnings("serial")
-public class Display extends JPanel{
+public class Display extends JPanel implements MemoriaObservador{
 	private final JLabel label;
- public Display() {
-	 label = new JLabel("0");
-	 add(label);
-	 label.setForeground(Color.WHITE);
-	 label.setFont(new Font("courier",Font.PLAIN,30));
-	 setBackground(new Color(46,49,50));
-	 setLayout(new FlowLayout(FlowLayout.RIGHT,10,25));
- }
+	 public Display() {
+		 Memoria.getInstancia().adicionarObservador(this);
+		 label = new JLabel(Memoria.getInstancia().getTextoAtual());
+		 add(label);
+		 label.setForeground(Color.WHITE);
+		 label.setFont(new Font("courier",Font.PLAIN,30));
+		 setBackground(new Color(46,49,50));
+		 setLayout(new FlowLayout(FlowLayout.RIGHT,10,25));
+	 }
+	 public void valorAlterado(String novoValor) {
+		 label.setText(novoValor);
+	 }
 }
